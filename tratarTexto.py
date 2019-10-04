d41b11341572d2aa77f297adcb75a777ffccf9be
= <=>
2019-10-04 10:37:14 +0200

    Tratamiento de texto parcial de candidaturas

--- tratarTexto.py
+++ tratarTexto.py
@@ -1,9 +1,16 @@
 import csv, operator
-file = open('Representantes.csv')
-entrada = csv.reader(file, delimiter = ';')
+import codecs
+file = codecs.open('Representantes.csv', 'r','utf-8')
+entrada = csv.reader(file, delimiter=';')
 for row in entrada:
+
     candidatura, provincia, candidatos, nombreyApellidos=row
-    print ("Candidatura " + candidatura, "Provincia "+provincia, "Nombre "+ nombreyApellidos)
+    if candidatura !='':
+        candidaturaNew = candidatura
+    if provincia !='':
+        provinciaNew = provincia
+
+    print ("Candidatura " + candidaturaNew, "Provincia "+provinciaNew, "Nombre "+ nombreyApellidos)
 
 file.close()
 """
